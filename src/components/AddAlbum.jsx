0444a36c88d9416ac78b4cddad237dc8328708f2
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-01 15:33:04 -0400

    form can submit new albums to db

--- src/components/AddAlbum.jsx
+++ src/components/AddAlbum.jsx
@@ -3,28 +3,48 @@ import { useState } from "react"
 export default function AddAlbum() {
   const [album, setAlbum] = useState('')
   const [artist, setArtist] = useState('')
-  const [year, setYear] = useState(1960)
+  const [year, setYear] = useState(1970)
   const handleSubmit = (e) => {
     e.preventDefault()
+    // check that all fields are filled
+    if(!album || !artist) {
+      alert('Please fill in all fields')
+      return
+    }
+    const newAlbum = { artist, album, year }
+    fetch('https://albums-api-tj.web.app/albums', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newAlbum)
+    })
+    .then(() => {
+      setAlbum('')
+      setArtist('')
+      setYear(1970)
+    })
+    .catch(alert)
+    
   }
   return (
     <section className="add-album">
       <h3>Add New Album</h3>
       <form onSubmit={handleSubmit}>
         <label htmlFor="album">Album:
-          <input type="text" name="album"
+          <input type="text" name="album" required
             onChange={e => setAlbum(e.target.value)}
             value={album} />
         </label>
         <br />
         <label htmlFor="artist">Artist:
-          <input type="text" name="artist"
+          <input type="text" name="artist" required
             onChange={e => setArtist(e.target.value)}
             value={artist} />
         </label>
         <br />
         <label htmlFor="year">Year:
-          <input type="number" name="year"
+          <input type="number" name="year" required
             onChange={e => setYear(e.target.value)}
             value={year} />
         </label>
