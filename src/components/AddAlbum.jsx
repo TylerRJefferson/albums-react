33f07d8e70fbac94c576fb2049bbb8080b3228b6
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-01 15:57:13 -0400

    toggle added to auto show new albums on page

--- src/components/AddAlbum.jsx
+++ src/components/AddAlbum.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react"
 
-export default function AddAlbum() {
+export default function AddAlbum({ setToggle, toggle }) {
   const [album, setAlbum] = useState('')
   const [artist, setArtist] = useState('')
   const [year, setYear] = useState(1970)
   const handleSubmit = (e) => {
     e.preventDefault()
     // check that all fields are filled
-    if(!album || !artist) {
+    if (!album || !artist || !year) {
       alert('Please fill in all fields')
       return
     }
@@ -19,13 +19,13 @@ export default function AddAlbum() {
       },
       body: JSON.stringify(newAlbum)
     })
-    .then(() => {
-      setAlbum('')
-      setArtist('')
-      setYear(1970)
-    })
-    .catch(alert)
-    
+      .then(() => {
+        setToggle(!toggle)
+        setAlbum('')
+        setArtist('')
+        setYear(1970)
+      })
+      .catch(alert)
   }
   return (
     <section className="add-album">
